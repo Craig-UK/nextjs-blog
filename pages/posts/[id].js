db63574faf2414849ecbf4db03cbd586205f8c29
Craig-UK <ScytherHQ@gmail.com>
2023-01-15 00:40:03 +0000

    Add 'getStaticPaths()' function to 'pages/posts/[id].js' file/component to return the array of known paths returned by the 'getAllPostIds()' function.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,14 @@
 import Layout from '../../components/layout';
+import { getAllPostIds } from '../../lib/posts';
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds(); // Contains the array of known paths returned by 
+                                // "getAllPostIds()".
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export default function Post() {
   return <Layout>...</Layout>;
